e42a592c103e101fe5cf0f341276622b9075d272
labeneko <takahiro_tsuno@dwango.co.jp>
2015-02-09 18:40:47 +0900

    MainActivityをHomeActivityに変更

---
+++ app/src/main/java/com/example/takahiro_tsuno/newsreader/ui/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.takahiro_tsuno.newsreader.ui.activity;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import com.example.takahiro_tsuno.newsreader.R;
+
+
+public class HomeActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
